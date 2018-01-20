e8230087c862546b7d55646c1e15496c446254a6
Ronnie Chong <hello@ronniechong.com>
2018-01-21 09:00:29 +1100

    Use process.cwd instead of __dirname

--- app/server/index.js
+++ app/server/index.js
@@ -4,11 +4,11 @@ const Site = require('./site');
 const Geolocation = require('./geolocation');
 
 const app = express();
-
+const cwd = process.cwd();
 app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, '/../html'));
-app.use('/static', express.static(path.join(__dirname, '/../static')));
-app.use('/scripts', express.static(path.join(__dirname, '/../scripts')));
+app.set('views', path.join(cwd, 'app/html'));
+app.use('/static', express.static(path.join(cwd, 'app/static')));
+app.use('/scripts', express.static(path.join(cwd, 'app/scripts')));
 app.use('/', Site);
 app.use('/api/v1/location/', Geolocation);
 
