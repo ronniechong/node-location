3a045c2657d43432320acd89ae23088ea6ba05b9
Ronnie Chong <hello@ronniechong.com>
2018-01-21 09:28:29 +1100

    Explicit port assignment

--- app/server/index.js
+++ app/server/index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const path = require('path');
 const Site = require('./site');
 const Geolocation = require('./geolocation');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 const cwd = process.cwd();
+app.set('port', (process.env.PORT || 5000));
 app.set('view engine', 'pug');
 app.set('views', path.join(cwd, 'app/html'));
 app.use('/static', express.static(path.join(cwd, 'app/static')));
