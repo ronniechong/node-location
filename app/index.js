3ba149401f537db54078f8dc47ef9d065e09f224
Ronnie Chong <hello@ronniechong.com>
2018-01-21 15:18:56 +1100

    Use PORT instead of SERVERPORT

--- app/index.js
+++ app/index.js
@@ -4,9 +4,9 @@ const path = require('path');
 
 dotenv.config();
 
-const port = process.env.SERVERPORT || 8080;
+const port = process.env.PORT || 8080;
 const host = process.env.SERVERHOST || '0.0.0.0';
-app.listen(port, host, () => {
+const server = app.listen(port, host, () => {
   /* eslint-disable */
   console.log('Express server listening on port', port);
   const cwd = process.cwd();
