6646e60429ba737d4192c9bb7f70ee9bf242375a
Ronnie Chong <hello@ronniechong.com>
2018-01-21 14:48:20 +1100

    Test

--- app/index.js
+++ app/index.js
@@ -8,5 +8,27 @@ const host = process.env.SERVERHOST || '0.0.0.0';
 app.listen(port, host, () => {
   /* eslint-disable */
   console.log('Express server listening on port', port);
+  const cwd = process.cwd();
+  console.log(cwd);
   /* eslint-enable */
+});
+
+app.get('/', (req, res) => {
+  const cwd = process.cwd();
+  console.log(cwd);
+  res.render('index', {
+    title: 'My Home',
+    message: 'Geolocation',
+    gmapApi: process.env.GMAPAPI,
+    apiUrl: process.env.GEOLOCATIONADDURL,
+    clientId: process.env.GAPPCLIENTID,
+  });
+});
+
+app.get('/signin', (req, res) => {
+  res.render('signin', {
+    title: 'My Home',
+    message: 'Please sign in to proceed',
+    clientId: process.env.GAPPCLIENTID,
+  });
 });
--- app/server/index.js
+++ app/server/index.js
@@ -13,7 +13,7 @@ app.set('view engine', 'pug');
 app.set('views', path.join(cwd, 'app/html'));
 app.use('/static', express.static(path.join(cwd, 'app/static')));
 app.use('/scripts', express.static(path.join(cwd, 'app/scripts')));
-app.use('/', Site);
+// app.use('/', Site);
 app.use('/api/v1/location/', Geolocation);
 
 module.exports = app;
