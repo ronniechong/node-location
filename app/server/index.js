3c90db1fec9de0b6624e8bb3b44a57d4f0f78989
Ronnie Chong <hello@ronniechong.com>
2018-01-21 15:02:37 +1100

    Add slash

--- app/server/index.js
+++ app/server/index.js
@@ -10,9 +10,9 @@ const app = express();
 const cwd = process.cwd();
 app.set('port', (process.env.PORT || 5000));
 app.set('view engine', 'pug');
-app.set('views', path.join(cwd, 'app/html'));
-app.use('/static', express.static(path.join(cwd, 'app/static')));
-app.use('/scripts', express.static(path.join(cwd, 'app/scripts')));
+app.set('views', path.join(cwd, '/app/html'));
+app.use('/static', express.static(path.join(cwd, '/app/static')));
+app.use('/scripts', express.static(path.join(cwd, '/app/scripts')));
 // app.use('/', Site);
 // app.use('/api/v1/location/', Geolocation);
 
